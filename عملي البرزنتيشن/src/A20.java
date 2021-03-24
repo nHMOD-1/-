105be5dbd77ccf57d902968e047d0554d551808f
kewpf <kewpf@DESKTOP-F8DVBEC>
2021-03-24 14:57:39 +0300

    Ferst commit

---
+++ عملي البرزنتيشن/src/A20.java
@@ -0,0 +1,15 @@
+
+public class A20 {
+ 
+    // ÚäÏ ÅÓÊÏÚÇÁåÇ ÊØÈÚ ÌãáÉ ááÊÑÍíÈ .welcomeMessage åäÇ ŞãäÇ ÈÊÚÑíİ ÏÇáÉ ÅÓãåÇ
+    public static void welcomeMessage() {
+        System.out.println("Welcome ");
+    }
+ 
+ 
+    public static void main(String[] args) {
+        // áØÈÇÚÉ ÌãáÉ ÇáÊÑÍíÈ ÇáãæÖæÚÉ İíåÇ welcomeMessage åäÇ ŞãäÇ ÈÇÓÊÏÚÇÁ ÇáÏÇáÉ
+        welcomeMessage();
+    }
+ 
+}
---
+++ عملي البرزنتيشن/src/A21.java
@@ -0,0 +1,14 @@
+public class A21 {
+ 
+    // ÚäÏ ÅÓÊÏÚÇÁåÇ äÚØíåÇ ÚÏÏíä İÊÑÌÚ äÇÊÌ ÌãÚ åĞíä ÇáÚÏÏíä sum åäÇ ŞãäÇ ÈÊÚÑíİ ÏÇáÉ ÅÓãåÇ
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+ 
+ 
+    public static void main(String[] args) {
+        // áÍÓÇÈ äÇÊÌ ÌãÚ ÇáÚÏÏíä 5 æ 10 sum åäÇ ŞãäÇ ÈÇÓÊÏÚÇÁ ÇáÏÇáÉ
+        System.out.println( "5 + 10 = " + sum(10, 5) );
+    }
+ 
+}
